c8b29dddecee0c6d3a486ab216641e280c8d0a44
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-20 10:54:58 -0300

    cdn requester: add chunks on storage

--- src/cdn_requester.js
+++ src/cdn_requester.js
@@ -5,20 +5,24 @@
 
 var BaseObject = require('base_object');
 var Utils = require('./utils');
+var Storage = require('./storage')
 
 class CDNRequester extends BaseObject {
   get name() { return 'ChunksHandler'; }
   initialize() {
+    this.storage = Storage.getInstance()
     this.utils = new Utils()
   }
 
   requestResource(resource, callback) {
     this.callback = callback
+    this.resource = resource
     this.utils.request(resource, (event) => this.resourceLoaded(event), 'arraybuffer')
   }
 
   resourceLoaded(event) {
     var chunk = this.utils.base64ArrayBuffer(event.currentTarget.response)
+    this.storage.setItem(this.resource, chunk)
     this.callback(chunk);
   }
 }
