6e98a1afec667d801068113cc5fb49cca36cc977
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-20 10:54:47 -0300

    add settings and storage

---
+++ src/settings.js
@@ -0,0 +1,11 @@
+// Copyright 2014 Flávio Ribeiro <flavio@bem.tv>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+class Settings {
+}
+
+Settings.maxStorageBytes = 300000 * 3
+
+module.exports = Settings
---
+++ src/storage.js
@@ -0,0 +1,60 @@
+// Copyright 2014 Flávio Ribeiro <flavio@bem.tv>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var Settings = require("./settings")
+var _ = require("underscore")
+
+class Storage {
+  constructor() {
+    // making all operations in associative arrays
+    // to avoid unecessary operations in a big structure
+    this.keys = []
+    this.values = []
+    this.totalBytes = 0
+  }
+
+  setItem(key, value) {
+    this.keys.push(key)
+    this.values.push(value)
+    this.totalBytes += value.length
+    if (this.totalBytes > Settings.maxStorageBytes) {
+      this.removeOlderItem()
+    }
+  }
+
+  removeOlderItem() {
+    this.keys.splice(0, 1)
+    this.values.splice(0, 1)
+  }
+
+  getItem(key) {
+    var index = this.keys.indexOf(key)
+    if (index > 0) {
+      return this.values[index]
+    }
+    return ""
+  }
+
+  clear() {
+    this.keys = []
+    this.values = []
+    this.totalBytes = 0
+  }
+
+  length() {
+    return this.keys.length
+  }
+}
+
+Storage.getInstance = function() {
+  if (this._instance == undefined) {
+    this._instance = new this();
+    this.chunks = {}
+  }
+  return this._instance;
+}
+
+module.exports = Storage
+
