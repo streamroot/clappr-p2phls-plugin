1f77a60fdd22ac3659743fa4a8963bb1d890d5db
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-23 11:43:08 -0300

    settings: document properties

--- src/settings.js
+++ src/settings.js
@@ -6,10 +6,27 @@
 class Settings {
 }
 
-Settings.maxStorageBytes = 5 * 1024 * 1024 // 5 megabytes
+/* maxStorageBytes
+Maximum size of the storage in bytes. */
+Settings.maxStorageBytes = 5 * 1024 * 1024
+
+/* swarm
+Name of the swarm where peers converge. */
 Settings.swarm = 'bemtv-swarm'
+
+/* tracker
+Place where a rtc-switchboard server is running */
 Settings.tracker = 'http://server.bem.tv:8080'
+
+/* timeout
+Time in seconds that the player will wait for peers to
+send chunks. */
 Settings.timeout = 2000
+
+/* lowBufferLength
+Local buffer threshold in seconds in which the player
+will try to use P2P. Smaller than that, player will
+behave as a normal one. */
 Settings.lowBufferLength = 5
 
 module.exports = Settings
