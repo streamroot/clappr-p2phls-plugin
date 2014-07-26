a4e3ea50da4f5f7e3d9dfd91127c50ad970abc96
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-26 01:26:47 -0300

    storage: simplify the structure, still having stuck moments (refs #15)

--- src/storage.js
+++ src/storage.js
@@ -8,16 +8,14 @@ var _ = require("underscore")
 
 class Storage {
   constructor() {
-    // making all operations in associative arrays
-    // to avoid unecessary operations in a big structure
     this.keys = []
-    this.values = []
+    this.chunks = {}
     this.totalBytes = 0
   }
 
   setItem(key, value) {
     this.keys.push(key)
-    this.values.push(value)
+    this.chunks[key] = value
     this.totalBytes += value.length
     if (this.totalBytes > Settings.maxStorageBytes) {
       this.removeOlderItem()
@@ -25,37 +23,23 @@ class Storage {
   }
 
   removeOlderItem() {
-    this.keys.splice(0, 1)
-    this.values.splice(0, 1)
+    var key = this.keys.splice(0, 1)[0]
+    this.totalBytes -= this.chunks[key].length
+    delete this.chunks[key]
   }
 
   getItem(key) {
-    var index = this.keys.indexOf(key)
-    if (index > 0) {
-      return this.values[index]
-    }
-    return ""
-  }
-
-  clear() {
-    this.keys = []
-    this.values = []
-    this.totalBytes = 0
-  }
-
-  length() {
-    return this.keys.length
+    return this.chunks[key];
   }
 
   contain(key) {
-    return this.keys.indexOf(key) >= 0? true: false
+    return _.contains(this.keys, key)
   }
 }
 
 Storage.getInstance = function() {
   if (this._instance == undefined) {
     this._instance = new this();
-    this.chunks = {}
   }
   return this._instance;
 }
