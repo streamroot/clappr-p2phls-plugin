3db06f2d20eae5f6dbb61ee8f1aa1f181262e4d6
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-03 14:40:52 -0300

    peer: add ping?pong! to calculate RTT (close #12, refs #29)

--- src/peer.js
+++ src/peer.js
@@ -16,9 +16,24 @@ class Peer extends BaseObject {
     this.dataChannel.on("data", (data) => this.messageReceived(data))
     this.active = true
     this.score = 1000
+    this.sendPing()
   }
 
- send(command, resource, content='') {
+  sendPing() {
+    this.pingSentTime = Date.now()
+    this.dataChannel.send("ping$$")
+  }
+
+  sendPong() {
+    this.dataChannel.send("pong$$")
+  }
+
+  calculateRTT(command) {
+    this.rtt = Date.now() - this.pingSentTime
+    console.log(this.ident + ': ping?pong! rtt: ' + this.rtt)
+  }
+
+  send(command, resource, content='') {
     var message = this.mountMessage(command, resource, content)
     this.dataChannel.send(message)
   }
@@ -42,13 +57,12 @@ class Peer extends BaseObject {
       this.swarm.chunksSent += 1
     } else if (command === 'choke') {
       this.swarm.chokeReceived(resource)
+    } else if (command === 'ping') {
+      this.sendPong()
+    } else if (command === 'pong') {
+      this.calculateRTT()
     } else if (command === 'satisfy') {
       this.swarm.resourceReceived(this.ident, resource, content)
-//    } else if (this.storage.keys.length > 0) {
-//      var res = resource.split("/")[6]
-//      var first = this.storage.keys[0].split("_")[3]
-//      var lastChunk = this.storage.keys[this.storage.keys.length-1].split("_")[3]
-//      console.log("discarted: ", command, res, first, lastChunk)
     }
   }
 
