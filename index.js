3706b9affcdec8d504ce15a49bc34b25bef4117f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-05 00:12:11 -0300

    add Apache license on index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+// Copyright 2014 Flávio Ribeiro <flavio@bem.tv>.
+// All rights reserved.
+// Use of this source code is governed by Apache
+// license that can be found in the LICENSE file.
+//
 module.exports = require("./src/main");
