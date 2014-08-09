9a9dab269dfe0f1f7e35ca211c2de6e35ef47be3
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-09 19:39:51 -0300

    peer: adding debug log for every command received

--- src/peer.js
+++ src/peer.js
@@ -61,6 +61,7 @@ class Peer extends BaseObject {
 
   processMessage(data) {
     var [command, resource, content] = data.split("$")
+    log.debug('received _' + command + '_')
     if (command === 'interested') {
       this.interestedReceived(resource)
     } else if (command === "contain") {
