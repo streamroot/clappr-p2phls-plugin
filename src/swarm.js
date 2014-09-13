4d52053f8e138793bbb9ace66d1ef2ccdbd9c22f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-09-13 04:44:51 -0300

    swarm: split functions, create SwarmUtils

--- src/swarm.js
+++ src/swarm.js
@@ -3,16 +3,18 @@
 // Use of this source code is governed by a Apache
 // license that can be found in the LICENSE file.
 
-var BaseObject = require('base_object');
-var BufferedChannel = require('rtc-bufferedchannel');
+var BaseObject = require('base_object')
+var BufferedChannel = require('rtc-bufferedchannel')
 var Peer = require('./peer')
 var Settings = require('./settings')
 var _ = require('underscore')
-var log = require('./log');
+var log = require('./log')
+var SwarmUtils = require('./swarm_utils')
 
 
 class Swarm extends BaseObject {
   constructor() {
+    this.utils = new SwarmUtils(this)
     this.peers = []
     this.satisfyCandidate = undefined
     this.chunksSent = 0
@@ -34,58 +36,17 @@ class Swarm extends BaseObject {
   }
 
   removePeer(id) {
-    var peer = this.findPeer(id)
+    var peer = this.utils.findPeer(id)
     this.peers = _.without(this.peers, peer)
     log.info("quit: " + id + " (remains: " + this.size() + ")")
     this.trigger('swarm:sizeupdate', {swarmSize: this.size()})
   }
 
-  findPeer(id) {
-    return _.find(this.peers, function (peer) {
-      return (peer.ident === id)
-    }, this)
-  }
-
-  updatePeersScore() {
-    var successPeer = this.findPeer(this.satisfyCandidate)
-    var goodPeers = _.union([successPeer], this.peersContainsResource)
-    var badPeers = _.difference(this.contributors, goodPeers)
-    log.info("contributors good: " + goodPeers.length)
-    this.incrementScore(goodPeers)
-    this.decrementScore(badPeers)
-  }
-
-  incrementScore(peers) {
-    this.changeScore(peers, Settings.points)
-  }
-
-  decrementScore(peers) {
-    this.changeScore(peers, Settings.points * -1)
-  }
-
-  changeScore(peers, points) {
-    _.each(peers, function(peer) { peer.score += points }, this)
-  }
-
-  get contributors() {
-    var orderedPeers = _.sortBy(this.peers, function (p) { return p.score }).reverse()
-    if (_.size(this.peers) > Settings.maxContributors) {
-      var slice = orderedPeers.slice(0, Settings.maxContributors)
-      return slice
-    } else {
-      return orderedPeers
-    }
-  }
-
-  getLowestScorePeer() {
-    return _.first(_.sortBy(this.peers, function(p) { return p.score }))
-  }
-
   sendTo(recipients, command, resource, content='') {
     if (recipients === 'contributors') {
-      _.each(this.contributors, function(peer) { peer.send(command, resource, content) }, this)
+      _.each(this.utils.contributors, function(peer) { peer.send(command, resource, content) }, this)
     } else {
-      var peer = this.findPeer(recipients)
+      var peer = this.utils.findPeer(recipients)
       peer.send(command, resource, content);
     }
   }
@@ -95,7 +56,7 @@ class Swarm extends BaseObject {
     this.externalCallbackSuccess = callbackSuccess
     this.currentResource = resource
     this.sendTo('contributors', 'interested', resource)
-    var timeout = this.getTimeoutFor('interested')
+    var timeout = this.utils.timeoutFor('interested')
     this.interestedFailID = setTimeout(this.callbackFail.bind(this), timeout)
   }
 
@@ -103,7 +64,7 @@ class Swarm extends BaseObject {
     if (this.currentResource === resource) {
       this.chokedClients += 1
     }
-    if (this.chokedClients === _.size(this.contributors)) {
+    if (this.chokedClients === _.size(this.utils.contributors)) {
       log.warn("all contributors choked, getting from cdn")
       this.callbackFail()
     }
@@ -117,7 +78,7 @@ class Swarm extends BaseObject {
     } else {
       this.satisfyCandidate = peer.ident
       this.clearInterestedFailInterval()
-      this.requestFailID = setTimeout(this.callbackFail.bind(this), this.getTimeoutFor('request'))
+      this.requestFailID = setTimeout(this.callbackFail.bind(this), this.timeoutFor('request'))
       this.sendTo(this.satisfyCandidate, 'request', resource)
     }
   }
@@ -133,29 +94,19 @@ class Swarm extends BaseObject {
   }
 
   busyReceived(peer) {
-    var lowerScore = this.getLowestScorePeer().score
+    var lowerScore = this.utils.getLowestScorePeer().score
     if (lowerScore < 0) {
       peer.score = lowerScore - Settings.points
     } else {
       peer.score = 0
     }
-    log.warn("busy received, changing " + peer.ident + " score to: " + peer.score)
+    log.warn("busy received. " + peer.ident + " score is now: " + peer.score)
   }
 
   callbackFail() {
-    this.decrementScore(this.contributors)
+    this.decrementScore(this.utils.contributors)
     this.rebootRoundVars()
     this.externalCallbackFail()
-  }
-
-  getTimeoutFor(command) {
-    var segmentSize = this.avgSegmentSize > 0? this.avgSegmentSize * 1000: 1000
-    if (command === 'interested') {
-      var timeout = segmentSize / 4
-      return timeout > 2000? 2000: timeout
-    } else if (command === 'request') {
-      return segmentSize * 0.6
-    }
   }
 
   rebootRoundVars() {
---
+++ src/swarm_utils.js
@@ -0,0 +1,68 @@
+// Copyright 2014 Flávio Ribeiro <flavio@bem.tv>.
+// All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+var _ = require('underscore')
+var Settings = require('./settings')
+var log = require('./log')
+
+
+class SwarmUtils extends BaseObject {
+  constructor(swarm) {
+    this.swarm = swarm
+  }
+
+  findPeer(id) {
+    return _.find(this.swarm.peers, function (peer) {
+      return (peer.ident === id)
+    }, this)
+  }
+
+  updatePeersScore() {
+    var successPeer = this.findPeer(this.utils.satisfyCandidate)
+    var goodPeers = _.union([successPeer], this.utils.peersContainsResource)
+    var badPeers = _.difference(this.contributors, goodPeers)
+    log.info("contributors good: " + goodPeers.length)
+    this.incrementScore(goodPeers)
+    this.decrementScore(badPeers)
+  }
+
+  incrementScore(peers) {
+    this.changeScore(peers, Settings.points)
+  }
+
+  decrementScore(peers) {
+    this.changeScore(peers, Settings.points * -1)
+  }
+
+  changeScore(peers, points) {
+    _.each(peers, function(peer) { peer.score += points }, this)
+  }
+
+  get contributors() {
+    var orderedPeers = _.sortBy(this.swarm.peers, function (p) { return p.score }).reverse()
+    if (_.size(this.swarm.peers) > Settings.maxContributors) {
+      var slice = orderedPeers.slice(0, Settings.maxContributors)
+      return slice
+    } else {
+      return orderedPeers
+    }
+  }
+
+  getLowestScorePeer() {
+    return _.first(_.sortBy(this.swarm.peers, function(p) { return p.score }))
+  }
+
+  timeoutFor(command) {
+    var segmentSize = this.swarm.avgSegmentSize > 0? this.swarm.avgSegmentSize * 1000: 1000
+    if (command === 'interested') {
+      var timeout = segmentSize / 4
+      return timeout > 2000? 2000: timeout
+    } else if (command === 'request') {
+      return segmentSize * 0.6
+    }
+  }
+}
+
+module.exports = SwarmUtils
