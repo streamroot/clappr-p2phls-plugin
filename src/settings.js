c7186f99cbdcbfd5f104fbe6d9bbf6f32ee7afac
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-11-12 21:11:05 -0200

    settings: remove google stun servers

--- src/settings.js
+++ src/settings.js
@@ -53,12 +53,7 @@ module.exports = {
   /* stunServers
   STUN servers used to match peers. */
   stunServers: [
-    {"url": "stun:stun.bem.tv:3478"},
-    {"url": "stun:stun.l.google.com:19302"},
-    {"url": "stun:stun1.l.google.com:19302"},
-    {"url": "stun:stun2.l.google.com:19302"},
-    {"url": "stun:stun3.l.google.com:19302"},
-    {"url": "stun:stun4.l.google.com:19302"}
+    {"url": "stun:stun.bem.tv:3478"}
   ],
 
   /* forceAllowCredentials
