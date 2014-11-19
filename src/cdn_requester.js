e50c468b8859f0273b4ccb4e97abf0fd9a433566
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-11-19 15:23:38 -0200

    cdn request: retry on xhr error (close #128)

--- src/cdn_requester.js
+++ src/cdn_requester.js
@@ -5,6 +5,7 @@
 
 var BaseObject = require('base_object')
 var Settings = require('./settings')
+var log = require('./log').getInstance()
 
 class CDNRequester extends BaseObject {
   get name() { return 'CDNRequester' }
@@ -14,7 +15,8 @@ class CDNRequester extends BaseObject {
   }
 
   getWorkerURL() {
-    var content = 'request=function(e,r,s,n){var t=new XMLHttpRequest;return r&&(t.withCredentials=!0),t.open("GET",e,s?!0:!1),n&&(t.responseType=n),s?(t.onload=s,void t.send()):(t.send(),200==t.status?t.response:"")},base64ArrayBuffer=function(e){for(var r,s,n,t,a,o="",u="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",d=new Uint8Array(e),i=d.byteLength,f=i%3,l=i-f,p=0;l>p;p+=3)a=d[p]<<16|d[p+1]<<8|d[p+2],r=(16515072&a)>>18,s=(258048&a)>>12,n=(4032&a)>>6,t=63&a,o+=u[r]+u[s]+u[n]+u[t];return 1==f?(a=d[l],r=(252&a)>>2,s=(3&a)<<4,o+=u[r]+u[s]+"=="):2==f&&(a=d[l]<<8|d[l+1],r=(64512&a)>>10,s=(1008&a)>>4,n=(15&a)<<2,o+=u[r]+u[s]+u[n]+"="),o},resourceLoaded=function(e){var r=base64ArrayBuffer(e.currentTarget.response);this.postMessage(r)},this.addEventListener("message",function(e){var r=JSON.parse(e.data);request(r.url,r.allowCredentials,resourceLoaded.bind(this),"arraybuffer")},!1);'
+    // non-minified version at https://gist.github.com/flavioribeiro/d706afbf83d055439f21
+    var content = 'request=function(e,r,t){var n=new XMLHttpRequest;r&&(n.withCredentials=!0),n.open("GET",e,t?!0:!1),n.responseType="arraybuffer",n.onload=function(e){t(200===n.status?e.currentTarget.response:"")},n.send()},base64ArrayBuffer=function(e){for(var r,t,n,s,a,o="",u="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",i=new Uint8Array(e),d=i.byteLength,f=d%3,c=d-f,p=0;c>p;p+=3)a=i[p]<<16|i[p+1]<<8|i[p+2],r=(16515072&a)>>18,t=(258048&a)>>12,n=(4032&a)>>6,s=63&a,o+=u[r]+u[t]+u[n]+u[s];return 1==f?(a=i[c],r=(252&a)>>2,t=(3&a)<<4,o+=u[r]+u[t]+"=="):2==f&&(a=i[c]<<8|i[c+1],r=(64512&a)>>10,t=(1008&a)>>4,n=(15&a)<<2,o+=u[r]+u[t]+u[n]+"="),o},resourceLoaded=function(e){var e=base64ArrayBuffer(e);this.postMessage(e)},this.addEventListener("message",function(e){var r=JSON.parse(e.data);request(r.url,r.allowCredentials,resourceLoaded.bind(this))},!1);'
     var blob
     try {
       blob = new Blob([content], {type: 'application/javascript'})
@@ -37,9 +39,14 @@ class CDNRequester extends BaseObject {
   }
 
   resourceLoaded(chunk) {
-    var downloadTime = Date.now() - this.startRequest
-    this.trigger('cdnrequester:downloadtime', {downloadTime: downloadTime, type: 'cdn'})
-    this.callback(chunk, "cdn");
+    if (chunk.length === 0) {
+      log.warn("error fetching from CDN, retrying")
+      this.requestResource(this.resource, this.callback)
+    } else {
+      var downloadTime = Date.now() - this.startRequest
+      this.trigger('cdnrequester:downloadtime', {downloadTime: downloadTime, type: 'cdn'})
+      this.callback(chunk, "cdn");
+    }
   }
 }
 
