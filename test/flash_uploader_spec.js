23e0ab541b1943962008ea6e57ab3b512fc69017
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-11-24 17:58:06 -0200

    flash_uploader: fix tests

--- test/flash_uploader_spec.js
+++ test/flash_uploader_spec.js
@@ -7,22 +7,24 @@ describe('FlashUploader', function() {
     this.sendPartCallback = function(subSegment) { this.receivedChunk += subSegment }.bind(this);
     this.finishCallback = function() {}.bind(this);
     this.flashUploader = new FlashUploader();
-    this.flashUploader.MAX_SIZE = 10;
+    this.flashUploader.MAX_SIZE = 100;
   })
 
   it('should send chunks smaller than MAX_SIZE', () => {
-    this.chunk = (new Array(this.flashUploader.MAX_SIZE - 1).join("x"));
+    var header = "d131dd02c5e6eec42fcab50712467eab"
+    this.chunk = header + (new Array(this.flashUploader.MAX_SIZE - 1).join("x"));
     this.flashUploader.send(this.sendPartCallback, this.chunk, this.finishCallback);
     this.clock.tick(100);
-    expect(this.receivedChunk).to.be.equal(this.chunk);
+    expect(this.receivedChunk).to.be.equal(this.chunk.slice(32));
   });
 
   it('should send chunks bigger than MAX_SIZE', () => {
-    this.chunk = (new Array(this.flashUploader.MAX_SIZE * 50).join("x"));
+    var header = "d131dd02c5e6eec42fcab50712467eab"
+    this.chunk = header + (new Array(this.flashUploader.MAX_SIZE * 50).join("x"));
     this.chunk += "xxx";
     this.flashUploader.send(this.sendPartCallback, this.chunk, this.finishCallback);
     this.clock.tick(100);
-    expect(this.receivedChunk).to.be.equal(this.chunk);
+    expect(this.receivedChunk).to.be.equal(this.chunk.slice(32));
   });
 
 });
